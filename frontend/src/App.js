fc3d9a696aba28cf7be52374988a979fb012030a
Breno Cupertino Dias <brenocuper@gmail.com>
2020-04-05 22:11:36 -0300

    First and only commit :)

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './global.css';
+
+import Routes from './routes';
+
+import Logon from './pages/Logon';
+
+function App() {
+  
+
+  return (
+    <Routes></Routes>
+  );
+}
+
+export default App;
